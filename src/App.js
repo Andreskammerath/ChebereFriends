f4c9f43423c9e64ba1cffc1d245727c504fa3c3a
tupapiloaleman <andreskammerath@gmail.com>
2019-10-28 17:06:29 -0300

    Scrollbar to scroll over cards component

--- src/App.js
+++ src/App.js
@@ -3,16 +3,20 @@ import logo from './logo.svg';
 import './App.css';
 import CardList from "./CardList";
 import SearchBox from "./SearchBox";
-import {friends} from './friends';
+import Scroll from './Scroll'
 class App extends React.Component{
 	constructor(){
 		super();
 		this.state = {
-			friends: friends,
+			friends: [],
 			searchfield:''
 		}
 	}
-
+	componentDidMount(){
+		fetch("https://jsonplaceholder.typicode.com/users")
+		.then(response =>{ return response.json();})
+		.then(users => {this.setState({friends: users})});
+	}
 	onSearchChange = (event) => {
 		this.setState({searchfield: event.target.value})
 	}
@@ -23,7 +27,9 @@ class App extends React.Component{
 	    	<div className="tc">
 	        	<h1 className="MarioF f1">Chebere friends</h1>
 	        	<SearchBox searchChange={this.onSearchChange}/>
-	        	<CardList friends={filteredfriends}/>
+	        	<Scroll>
+	        		<CardList friends={filteredfriends}/>
+	        	</Scroll>
 	    	</div>    
 	  	);
 	}  
