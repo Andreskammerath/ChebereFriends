bda0d768dc2900c1b8be13f39ed0ed0e40d4682d
tupapiloaleman <andreskammerath@gmail.com>
2019-10-30 20:14:25 -0300

    Updates

---
+++ precache-manifest.da5319297802aa256e014c5ccde7b603.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a7ae6ff040305540611c61ee1261995",
+    "url": "/ChebereFriends/index.html"
+  },
+  {
+    "revision": "f2c16011518d63d92a8d",
+    "url": "/ChebereFriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "327eb8bfc8847570973f",
+    "url": "/ChebereFriends/static/css/main.3a3d13c8.chunk.css"
+  },
+  {
+    "revision": "f2c16011518d63d92a8d",
+    "url": "/ChebereFriends/static/js/2.58593dce.chunk.js"
+  },
+  {
+    "revision": "327eb8bfc8847570973f",
+    "url": "/ChebereFriends/static/js/main.61ef6e80.chunk.js"
+  },
+  {
+    "revision": "3e9be27e754f66fc8fb7",
+    "url": "/ChebereFriends/static/js/runtime-main.72bafa9f.js"
+  },
+  {
+    "revision": "6c02f15fdbc9dd7c482b52b06d8e0a6c",
+    "url": "/ChebereFriends/static/media/SuperMario256.6c02f15f.ttf"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/ChebereFriends/static/media/logo.25bf045c.svg"
+  }
+]);
